12f056bbb11f5aaa123192d2023f047cdf7cda18
cancion97 <mikiasm1207@gmail.com>
2025-10-10 15:39:14 +0900

    004_20251010_ハンバーガーメニュー中身途中まで

--- js/script.js
+++ js/script.js
@@ -47,3 +47,34 @@ hbgButton.addEventListener("click", () => {
     hbgButton.classList.add("is-open");
   }
 });
+
+//横スクロール
+const slider = document.querySelector(".horizontal_scroll");
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener("mousedown", (e) => {
+  isDown = true;
+  slider.classList.add("active"); // grabbing に切替
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener("mouseleave", () => {
+  isDown = false;
+  slider.classList.remove("active"); // grab に戻す
+});
+
+slider.addEventListener("mouseup", () => {
+  isDown = false;
+  slider.classList.remove("active"); // grab に戻す
+});
+
+slider.addEventListener("mousemove", (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 1; // 1はスクロールスピード
+  slider.scrollLeft = scrollLeft - walk;
+});
